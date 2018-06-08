f0f2b861f182613348b7f62a3ac26ea5f0de8be9
fxfviolet <fxfviolet@163.com>
2018-06-08 15:50:53 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ eval_dir=$output_dir/eval
 
 # config文件
 config=ssd_mobilenet_v1_week9.config
-pipeline_config_path=$config
+pipeline_config_path=/tinysrc/$config
 
 # 先清空输出目录，本地运行会有效果，tinymind上运行这一行没有任何效果
 # tinymind已经支持引用上一次的运行结果，这一行需要删掉，不然会出现上一次的运行结果被清空的状况。
